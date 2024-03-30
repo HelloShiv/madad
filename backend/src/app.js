d6e2520fd53be64b7cb15de90f0bf4031947a3f5
HelloShiv <swagatswaroopparida@gmail.com>
2024-03-30 15:12:33 +0530

    adding vercel.json for backend deployment for vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
